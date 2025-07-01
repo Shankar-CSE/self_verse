85670720c5f02ef11a7f171d0bbfd12376704fe1
Shankar-CSE <shankar.l5252@gmail.com>
2025-07-01 12:24:20 +0530

    feat: integrate Hive for local storage and implement app start logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:self_verse/screens/getStarted.dart';
 import 'package:self_verse/screens/home.dart';
 
-void main() {
+void main() async {
+ WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  // Open a box for storing data
+  await Hive.openBox('getStartedBox');
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final _mybox = Hive.box("getStartedBox");
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Self verse',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Getstarted(),
+      home: _mybox.get("started") == null
+          ? const GetStarted()
+          : const Home(),
     );
   }
 }
